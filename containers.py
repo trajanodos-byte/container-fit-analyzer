ec543e696a98256247031230d878abcbf0a12d26
trajanodos-byte <trajanodos@gmail.com>
2026-06-04 10:19:37 -0400

    Create containers.py

---
+++ containers.py
@@ -0,0 +1,20 @@
+CONTAINERS = {
+    "20DC": {
+        "length": 233.7,
+        "width": 92.4,
+        "height": 94.5,
+        "payload": 28200
+    },
+    "40DC": {
+        "length": 473.7,
+        "width": 92.4,
+        "height": 94.5,
+        "payload": 26700
+    },
+    "40HC": {
+        "length": 473.7,
+        "width": 92.4,
+        "height": 106.3,
+        "payload": 26500
+    }
+}
